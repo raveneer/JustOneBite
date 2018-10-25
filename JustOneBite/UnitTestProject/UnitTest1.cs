49e626924bc51abd8bd59827e75c5a981c2d8ac4
joohokim <ravenerkr@gmail.com>
2018-10-25 21:31:56 +0900

    엔크런치와 엔유닛 추가.

---
+++ JustOneBite/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTestProject
+{
+    public class TestClass
+    {
+        [Test]
+        public void Test_First()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
