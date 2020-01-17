0d57c76468842e5c6b7549becfa8747e9477b433
jooho <ravenerkr@gmail.com>
2020-01-17 15:53:21 +0900

    -중간저장

--- JustOneBite/ChattingHabit/MainWindow.xaml.cs
+++ JustOneBite/ChattingHabit/MainWindow.xaml.cs
@@ -34,44 +34,45 @@ namespace ChattingHabit
         private readonly string _systemSaveFilePath = AppDomain.CurrentDomain.BaseDirectory + "ChattingHabitSystemSave.Json";
         private readonly SoundPlayer _timeOverSound = new SoundPlayer();
         private readonly WebPageMonitor _webPageMonitor = new WebPageMonitor();
-        public static int TICKSECONDS = 5;
+        public static int BaseMethodsUpdateSecPerLoop = 1;
         private static string SaveFileName = "ChattingHabitSave.Json";
 
         public MainWindow()
         {
-            //EventManager.ShowLogMessage += ChangeFeedBackBoxText;
+            EventManager.ShowLogMessage += msg => LogText.Text = msg; ;
             EventManager.PlayTimeOverSound += () => _timeOverSound.Play();
 
-            InitSounds();
-            InitializeComponent();
-            //LoadSystemSetting();
-            LoadMonitoringProcesses();
-           // ChangeTotalLimit(TotalTimeLimitMinute);
-            //ChangeSessionLimit(SessionTimeLimitMinute);
-           // ShowProcesses();
-            StartUpdate();
-            StartAutoSaveTick();
-           // AsyncLogURL();
+            //InitSounds();
+            InitializeComponent(); // WPF 자체함수. 건드리지 말 것.
+            StartUpdateEventLoop();
+            SiteMonitorLoop();
+            StartAutoSaveLoop();
         }
 
-        private void StartUpdate()
+        private void StartUpdateEventLoop()
         {
             var timer = new System.Windows.Threading.DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, TICKSECONDS);
-            timer.Tick += Update;
+            timer.Interval = new TimeSpan(0, 0, BaseMethodsUpdateSecPerLoop);
+            timer.Tick += BaseMethodsUpdate;
             timer.Start();
         }
-
-        private void StartAutoSaveTick()
+        
+        /// <summary>
+        /// 무거운 연산이므로 따로 돌린다. 느리게.
+        /// </summary>
+        private void SiteMonitorLoop()
         {
-            var autoSaveTimer = new System.Windows.Threading.DispatcherTimer();
-            autoSaveTimer.Interval = new TimeSpan(0, 0, AutoSaveTermSec);
-            autoSaveTimer.Tick += (sender, args) => SaveDataToFile();
-            autoSaveTimer.Start();
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, SiteMonitorSecPerLoop);
+            timer.Tick += (sender, args) => { GetUrlAndBlockAsync(); };
         }
 
-        //메인 루프.
-        private async void Update(object sender, EventArgs e)
+        public int SiteMonitorSecPerLoop = 5;
+
+        /// <summary>
+        /// 가벼운 연산들. 자주 돌린다.
+        /// </summary>
+        private async void BaseMethodsUpdate(object sender, EventArgs e)
         {
             ShowClock();
             //IfTimeOverResetUsedTime();
@@ -79,10 +80,20 @@ namespace ChattingHabit
             //_webPageMonitor.Tick();
 
             //ManagingProcessInfoText.Text = _processCollection.GetProcessesInfo();
-            
+
             //걍 불러버려
             GetUrlAndBlockAsync();
         }
+
+
+        private void StartAutoSaveLoop()
+        {
+            var autoSaveTimer = new System.Windows.Threading.DispatcherTimer();
+            autoSaveTimer.Interval = new TimeSpan(0, 0, AutoSaveTermSec);
+            autoSaveTimer.Tick += (sender, args) => SaveDataToFile();
+            autoSaveTimer.Start();
+        }
+
 
         private void InitSounds()
         {
@@ -124,18 +135,13 @@ namespace ChattingHabit
         }
         */
 
+        /*
         public static bool IsValidProcessName(string processName)
         {
             var processes = Process.GetProcesses();
             return processes.Any(x => x.ProcessName == processName);
-        }
+        }*/
         
-        /*
-        private void ChangeFeedBackBoxText(string messeage)
-        {
-            FeedBackText.Text = messeage;
-        }
-        */
 
         /*
         private void ChangeSessionLimit(int minute)
@@ -170,6 +176,7 @@ namespace ChattingHabit
             }
         }*/
 
+        /*
         private void LoadMonitoringProcesses()
         {
             if (File.Exists(_saveFilePath))
@@ -188,7 +195,7 @@ namespace ChattingHabit
                 _processCollection.Add("slack");
                 _processCollection.Add("discord");
             }
-        }
+        }*/
 
         /*
         private void LoadSystemSetting()
@@ -246,7 +253,7 @@ namespace ChattingHabit
 
         private void ShowClock()
         {
-            //ManagingProcessInfoText.Text = DateTime.Now.ToLongTimeString();
+            ManagingProcessInfoText.Text = DateTime.Now.ToLongTimeString();
         }
 
         /*
